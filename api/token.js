72752711a6cb8a579c8df1d2d86d5a49b1cb2362
helen545 <helen@ozannaeventrentals.com>
2026-05-01 16:06:46 -0500

    Update token.js

--- api/token.js
+++ api/token.js
@@ -1,1 +1,28 @@
 
+const twilio = require("twilio");
+
+module.exports = async function handler(req, res) {
+  const AccessToken = twilio.jwt.AccessToken;
+  const VoiceGrant = AccessToken.VoiceGrant;
+
+  const identity = "admin";
+
+  const voiceGrant = new VoiceGrant({
+    outgoingApplicationSid: process.env.TWILIO_TWIML_APP_SID,
+    incomingAllow: true,
+  });
+
+  const token = new AccessToken(
+    process.env.TWILIO_ACCOUNT_SID,
+    process.env.TWILIO_API_KEY_SID,
+    process.env.TWILIO_API_KEY_SECRET,
+    { identity }
+  );
+
+  token.addGrant(voiceGrant);
+
+  res.status(200).json({
+    identity,
+    token: token.toJwt(),
+  });
+};
